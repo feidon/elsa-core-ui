1a9b788d0e862498a8277d0ea81acaeeaa28cf3e
StanFei <stanfei@webim.com.tw>
2023-09-19 16:04:58 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -7,14 +7,8 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => tag.startsWith('elsa')
-        }
-      }
-    }),
-    vueJsx()
+    vue(),
+    vueJsx(),
   ],
   resolve: {
     alias: {
